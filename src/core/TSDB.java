8a050ef4157e93880855188e3978d53b9369212a
Chris Larsen <clarsen@yahoo-inc.com>
2015-05-24 15:11:20 -0700

    Fix for the TSDB class where the client wasn't passed properly to the UID classes.
    Signed-off-by: Chris Larsen <clarsen@yahoo-inc.com>
    
    Conflicts:
    	src/core/TSDB.java

--- src/core/TSDB.java
+++ src/core/TSDB.java
@@ -146,9 +146,9 @@ public final class TSDB {
     treetable = config.getString("tsd.storage.hbase.tree_table").getBytes(CHARSET);
     meta_table = config.getString("tsd.storage.hbase.meta_table").getBytes(CHARSET);
 
-    metrics = new UniqueId(client, uidtable, METRICS_QUAL, METRICS_WIDTH);
-    tag_names = new UniqueId(client, uidtable, TAG_NAME_QUAL, TAG_NAME_WIDTH);
-    tag_values = new UniqueId(client, uidtable, TAG_VALUE_QUAL, TAG_VALUE_WIDTH);
+    metrics = new UniqueId(this.client, uidtable, METRICS_QUAL, METRICS_WIDTH);
+    tag_names = new UniqueId(this.client, uidtable, TAG_NAME_QUAL, TAG_NAME_WIDTH);
+    tag_values = new UniqueId(this.client, uidtable, TAG_VALUE_QUAL, TAG_VALUE_WIDTH);
     compactionq = new CompactionQueue(this);
 
     if (config.hasProperty("tsd.core.timezone")) {
